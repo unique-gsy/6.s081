920e95209306336f53a36a4f0b90c79b7aceb29d
GSY <285618794@qq.com>
2022-04-27 04:15:19 +0000

    modify primes

--- user/primes.c
+++ user/primes.c
@@ -2,68 +2,84 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-#define STDIN_FILE_NO 0
-#define STDOUT_FILE_NO 1
-#define STDERR_FILE_NO 2
+#define Pread 0
+#define Pwrite 1
 
-#define PIPE_READ 0
-#define PIPE_WRITE 1
-
-void kids(int *bp) {
-    int number = 0;
-    read(bp[PIPE_READ], &number, sizeof(number));
-    if (number == -1) 
-        exit(0);
-    printf("primes %d\n", number);
-
-    int p2c[2];
-    if (pipe(p2c) < 0) {
-        fprintf(STDERR_FILE_NO, "pipe error.\n");
+int read_left_first(int left[2], int* number) {
+    if (read(left[Pread], number, sizeof(int)) != sizeof(int)) {
+        fprintf(2, "kid read error!");
+        return -1;
     }
+    if (*number != -1) {
+        printf("primes %d\n", *number);
+        return 0;
+    }
+    return -1;
+}
 
-    int pid = fork();
-    if (pid < 0) {
-        fprintf(STDERR_FILE_NO, "fork error.\n");
-    } else if (pid == 0) {
-        close(p2c[PIPE_WRITE]);
-        kids(p2c);
-        exit(0);
-    } else {
-        close(p2c[PIPE_READ]);
-        int tmp = 0;
-        while (tmp != -1) {
-            read(bp[PIPE_READ], &tmp, sizeof(tmp));
-            if (tmp % number != 0) {
-                write(p2c[PIPE_WRITE], &tmp, sizeof(tmp));
-            }
+void trans_data(int left[2], int right[2], int first) {
+    int number = first;
+    while (number != -1) {
+        if (read(left[Pread], &number, sizeof(number)) != sizeof(int)) {
+            fprintf(2, "kid read error!");
+            exit(1);
         }
-        int end = -1;
-        write(p2c[PIPE_WRITE], &end, sizeof(end));
-        wait(0);
+        if (number != -1 && number % first)
+            write(right[Pwrite], &number, sizeof(number));
+        
     }
-    exit(0);
+    write(right[Pwrite], &number, sizeof(number));
+}
+
+void kids(int *left) {
+    int number = 0;
+    close(left[Pwrite]);
+    if (read_left_first(left, &number) == 0) {
+        int right[2];
+        if (pipe(right) < 0) {
+            fprintf(2, "pipe error.\n");
+            exit(1);
+        }
+
+        int pid = fork();
+        if (pid < 0) {
+            fprintf(2, "fork error.\n");
+            close(right[Pwrite]);
+            close(right[Pread]);
+            exit(1);
+        } else if (pid == 0) {
+            kids(right);
+            exit(0);
+        } else {
+            trans_data(left, right, number);
+            close(left[Pread]);
+            wait(0);
+            exit(0);
+        }
+    }
+    exit(1);
 }
 
 int main(int argc, char const *argv[]) {
-    int p2c[2];
-    if (pipe(p2c) < 0) {
-        fprintf(STDERR_FILE_NO, "pipe errpr.\n");
+    int right[2];
+    if (pipe(right) < 0) {
+        fprintf(2, "pipe errpr.\n");
+        exit(1);
     }
 
     int pid = fork();
     if (pid < 0) {
-        fprintf(STDERR_FILE_NO, "fork error.\n");
-    } else if (pid == 0) {
-        close(p2c[PIPE_WRITE]);
-        kids(p2c);
-        exit(0);
+        fprintf(2, "fork error.\n");
+        exit(1);
+    } else if (pid == 0) {  //kid
+        kids(right);
     } else {
-        close(p2c[PIPE_READ]);
-        for (int i = 2; i < 35; ++i) {
-            write(p2c[PIPE_WRITE], &i, sizeof(i));
-        }
+        close(right[Pread]);
+        for (int i = 2; i <= 35; ++i)
+            write(right[Pwrite], &i, sizeof(i));
         int end = -1;
-        write(p2c[PIPE_WRITE], &end, sizeof(end));
+        write(right[Pwrite], &end, sizeof(end));
+        close(right[Pwrite]);
         wait(0);
     }
     exit(0);
